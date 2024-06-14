5ed357853cf31e4e5aa6a9c433b77209c41dab18
fjrodl <fjrodl # ule>
2024-06-14 13:14:29 +0200

    Updated new checker

--- complete_installation.sh
+++ complete_installation.sh
@@ -3,10 +3,32 @@
 
 STARS="**********************************************"
 
+ROS2_FOLDER_PATH=~/ros2_ws
+ROS2_SRC_PATH=$ROS2_FOLDER_PATH/src
+
+
+# Function to check if a ROS2  folder exists and create it if it doesn't
+ensure_directory_exists() {
+  local dir_path="$1"
+  
+  if [ -d "$dir_path" ]; then
+    echo "Directory $dir_path already exists."
+  else
+    echo "Directory $dir_path does not exist. Creating it now..."
+    mkdir -p "$dir_path"
+    if [ $? -eq 0 ]; then
+      echo "Directory $dir_path created successfully."
+    else
+      echo "Failed to create directory $dir_path."
+    fi
+  fi
+}
+
+
 function yasmin_installation(){
 
     # clone
-    cd ~/ros2_ws/src
+    cd $ROS2_SRC_PATH
     git clone https://github.com/uleroboticsgroup/yasmin.git
 
     # dependencies
@@ -20,9 +42,9 @@ function yasmin_installation(){
 function audio_common_installation(){
 
     # clone
-    cd ~/ros2_ws/src
+    cd $ROS2_SRC_PATH
     git clone https://github.com/mgonzs13/audio_common.git
-    sudo apt install portaudio19-dev
+    sudo apt install -y portaudio19-dev
     pip3 install -r audio_common/requirements.txt
 
 }
@@ -30,7 +52,7 @@ function audio_common_installation(){
 function llama_installation(){
 
     # clone
-    cd ~/ros2_ws/src
+    cd $ROS2_SRC_PATH
     git clone --recurse-submodules https://github.com/mgonzs13/llama_ros.git
     pip3 install -r llama_ros/requirements.txt
 
@@ -40,10 +62,10 @@ function llama_installation(){
 function tts_ros_installation(){
 
     # clone
-    cd ~/ros2_ws/src
+    cd $ROS2_SRC_PATH
     git clone https://github.com/mgonzs13/audio_common.git
     git clone https://github.com/mgonzs13/tts_ros.git
-    sudo apt install portaudio19-dev
+    sudo apt install -y portaudio19-dev
     pip3 install -r audio_common/requirements.txt
     pip3 install -r tts_ros/requirements.txt
 
@@ -53,7 +75,7 @@ function tts_ros_installation(){
 function whisper_installation(){
 
     #$ 
-    cd ~/ros2_ws/src
+    cd $ROS2_SRC_PATH
  
     git clone --recurse-submodules https://github.com/mgonzs13/whisper_ros.git
     pip3 install -r whisper_ros/requirements.txt
@@ -65,7 +87,7 @@ function whisper_installation(){
 
 function activate_nvidia(){
     
-    cd ~/ros2_ws/src/whisper_ros/whisper_ros
+    cd $ROS2_SRC_PATH/whisper_ros/whisper_ros
 
     VAR_ORIGINAL="#option(WHISPER_CUBLAS "whisper: support for cuBLAS" ON)"
     VAR_NEW="option(WHISPER_CUBLAS "whisper: support for cuBLAS" ON)"
@@ -74,7 +96,7 @@ function activate_nvidia(){
     sed -i '/$VAR_ORIGINAL/c\$VAR_NEW' CMakeLists.txt
     cat CMakeLists.txt | grep WHISPER_CUBLAS
 
-    cd ~/ros2_ws/src/llama_ros/llama_ros
+    cd $ROS2_SRC_PATH/llama_ros/llama_ros
     sed -i '/#option(LLAMA_CUDA "llama: use CUDA" ON)/c\option(LLAMA_CUDA "llama: use CUDA" ON))' CMakeLists.txt
     sed -i '/#add_compile_definitions(GGML_USE_CUDA)/c\add_compile_definitions(GGML_USE_CUDA))' CMakeLists.txt
 
@@ -82,11 +104,11 @@ function activate_nvidia(){
 
 function deactivate_nvidia(){
     
-    cd ~/ros2_ws/src/whisper_ros/whisper_ros
+    cd $ROS2_SRC_PATH/whisper_ros/whisper_ros
 
     sed -i '/option(WHISPER_CUBLAS "whisper: support for cuBLAS" ON)/c\#option(WHISPER_CUBLAS "whisper: support for cuBLAS" ON)' CMakeLists.txt
     
-    cd ~/ros2_ws/src/llama_ros/llama_ros
+    cd $ROS2_SRC_PATH/llama_ros/llama_ros
     sed -i '/option(LLAMA_CUDA "llama: use CUDA" ON)/c\#option(LLAMA_CUDA "llama: use CUDA" ON))' CMakeLists.txt
     sed -i '/add_compile_definitions(GGML_USE_CUDA)/c\#add_compile_definitions(GGML_USE_CUDA))' CMakeLists.txt
 
@@ -95,7 +117,7 @@ function deactivate_nvidia(){
 
 function compilation(){
     
-    cd ~/ros2_ws
+    cd $ROS2_FOLDER_PATH
     rosdep install --from-paths src --ignore-src -r -y
     colcon build
 
@@ -103,7 +125,7 @@ function compilation(){
 
 function clean(){
     
-    cd ~/ros2_ws
+    cd $ROS2_FOLDER_PATH
     rm -rf build install log
 
 }
@@ -114,6 +136,7 @@ echo -e "$STARS"
 echo "Starting Installation of ROS 2 chatbot"
 echo -e "$STARS"
 
+ensure_directory_exists $ROS2_SRC_PATH
 
 yasmin_installation
 
